4c0757720475fe332b38f83e9d66aa375e88a774
zgerbi <44167794+zgerbi@users.noreply.github.com>
2022-10-11 04:43:32 -0400

    Added working collectible function and basic collectible counter UI

--- Assets/Scripts/Starter Scripts/Managers/CollectibleManager.cs
+++ Assets/Scripts/Starter Scripts/Managers/CollectibleManager.cs
@@ -19,7 +19,7 @@ public class CollectibleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        collectibles.text = "Donuts " + collected;
+        collectibles.text = "Beats: " + collected;
     }
 
     public void Collected(int value)
